07348bc03ae86f17f067cb9e24ea9279e94fcc7a
biyash-1 <sthaalish39@gmail.com>
2025-02-23 21:49:10 +0545

    main

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import './assets/main.css'
 import 'primeicons/primeicons.css'
-import  router from './router/index'
+import router from './router/index'
+import Vue3Toastify from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
 const app = createApp(App)
+app.use(Vue3Toastify, {
+    autoClose: 3000, // Global default auto-close duration in milliseconds
+    position: 'top-right', // Default position for toasts
+    // Add other global options here
+  });
+  
 app.use(router)
 app.mount('#app')

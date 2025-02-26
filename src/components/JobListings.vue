1a7f38b610b1e021ae43d39804843cd062c196f3
biyash-1 <sthaalish39@gmail.com>
2025-02-26 19:10:58 +0545

    listing page

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { ref, defineProps } from "vue";
+import { ref, defineProps, onMounted } from "vue";
+import axios from "axios";
 import Joblisting from "./Joblisting.vue";
-import jobData from "../../src/jobs.json";
+import PulseLoader from "vue-spinner/src/PulseLoader.vue"
 
 // Define props
 const props = defineProps({
@@ -15,9 +16,21 @@ const props = defineProps({
   },
 });
 
-// Reactive variables for job listings
-const jobs = ref(jobData.jobs);
-const jobLimit = ref(props.limit || jobs.value.length);
+const jobs = ref([]);
+const loading = ref(true)
+const jobLimit = ref(props.limit);
+
+onMounted(async () => {
+  loading.value = true
+  try {
+    const response = await axios.get("/api/jobs");
+    jobs.value = response.data;
+    loading.value= false
+    
+  } catch (err) {
+    console.error("Error fetching jobs:", err);
+  }
+});
 
 // Function to show all jobs
 const showAllJobs = () => {
@@ -26,22 +39,27 @@ const showAllJobs = () => {
 </script>
 
 <template>
-  <Navbar />
-  <Hero />
-  <HomeCard />
+ 
+  
   <section>
     <div class="container-xl lg:container m-auto">
       <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">Browse Jobs</h2>
+      <div v-if = "loading" class="text-center">
+        <PulseLoader size="5" margin="auto" class="mx-auto text-3xl"/>
+
+      </div>
       <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        <!-- Vue automatically unwraps refs in templates -->
         <Joblisting v-for="job in jobs.slice(0, jobLimit)" :key="job.id" :job="job" />
       </div>
     </div>
   </section>
 
   <!-- View All Jobs Button -->
-  <section v-if="showButton && jobLimit !== jobs.length">
-    <button @click="showAllJobs" class="block w-40 mt-4 bg-gray-300 text-black  text-center mx-auto hover:bg-gray-700 p-2 rounded mb-2">
+  <section v-if="showButton && jobLimit < jobs.length" class="text-center mt-4">
+    <button 
+      @click="showAllJobs" 
+      class="w-40 bg-gray-300 text-black hover:bg-gray-700 p-2 rounded mb-2"
+    >
       View All Jobs
     </button>
   </section>

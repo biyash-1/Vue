1c6d62c31ee3cacadbf9631077294eed2dffeab1
biyash-1 <sthaalish39@gmail.com>
2025-02-26 19:11:23 +0545

    index file

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import {createRouter, createWebHistory} from 'vue-router' 
-
 import Homeview from '@/views/Homeview.vue'
 import Jobsview from '@/views/Jobsview.vue'
+import NotFound from '@/views/NotFound.vue'
+import Jobview from '@/views/Jobview.vue'
+import Addjobview from '@/views/Addjobview.vue'
+import Editjobview from '@/views/Editjobview.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,7 +18,31 @@ component:Homeview
             path: '/jobs',
             name: 'jobs',
             component: Jobsview
-        }
+        },
+        {
+  path : '/jobs/:id',
+  name: 'job',
+  component: Jobview
+        },
+        {
+            path: '/jobs/add',
+            name: 'addjob',
+            component: Addjobview
+        },
+
+        {
+            path: '/jobs/edit/:id',
+            name: "editjob",
+            component: Editjobview
+
+        },
+        {
+            path : '/:catchAll(.*)',
+            name: 'not-found',
+            component: NotFound
+        },
+
+
     ]
     
 })
